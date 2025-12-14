df996f0a799fc29123f190baf5ed9cb220571453
satadhi <haldersatadhi@gmail.com>
2025-12-14 11:25:58 +0530

    testing out react-query basics + adv

---
+++ react-query/src/App.tsx
@@ -0,0 +1,38 @@
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import "./App.css";
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const posts = [
+  { id: 1, title: "momo master" },
+  { id: 2, title: "Charls" },
+];
+function App() {
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    queryKey: ["post"],
+    queryFn: () => wait(1000).then(() => [...posts]),
+  });
+
+  const postMutation = useMutation({
+    mutationFn: async (title: string) => {
+      // await wait(1000);
+      return posts.push({ id: Date.now(), title });
+    },
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["post"] }),
+  });
+
+  if (postQuery.isPending) return <div> Loading ....</div>;
+  return (
+    <>
+      {postQuery.data?.map((ele) => (
+        <div key={ele.id}>{ele.title}</div>
+      ))}
+
+      <button onClick={() => postMutation.mutate(crypto.randomUUID())}>
+        Add new
+      </button>
+    </>
+  );
+}
+
+export default App;
